79cbafe8fd935b8903328ad2aeefbb7935c1e419
Preston Smith <xtrafear@gmail.com>
2014-09-05 20:46:10 -0500

    Fix2 lul

--- Source/Core/Core/HW/EXI_DeviceMemoryCard.cpp
+++ Source/Core/Core/HW/EXI_DeviceMemoryCard.cpp
@@ -121,7 +121,7 @@ CEXIMemoryCard::CEXIMemoryCard(const int index, bool gciFolder)
 
 	tSHSL = SystemTimers::GetTicksPerSecond() * 100ULL / 1000000000ULL;
 	tPP = SystemTimers::GetTicksPerSecond() * 1400ULL / 1000000ULL;
-	tSE = SystemTimers::GetTicksPerSecond() * 60ULL / 1000ULL;
+	tSE = SystemTimers::GetTicksPerSecond() * 90ULL / 1000ULL;
 
 	WARN_LOG(EXPANSIONINTERFACE, "EXIChannel[%d]: Timing Information (tSHSL: %llu, tPP: %llu, tSE: %llu)", card_index, tSHSL, tPP, tSE);
 }
@@ -305,7 +305,7 @@ void CEXIMemoryCard::SetCS(int cs)
 				int count = m_uPosition - 5;
 				int i=0;
 				status |= MC_STATUS_BUSY;
-				status &= ~MC_STATUS_READY;
+				//status &= ~MC_STATUS_READY;
 
 				while (count--)
 				{
